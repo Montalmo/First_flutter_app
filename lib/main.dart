aef2d13a9c9cfdf192f4af55f61912dbe5fbaa13
Montalmo <photowedg@gmail.com>
2022-01-19 23:52:05 +0200

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-              primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'My first application'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 2;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      //theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First Application', style: TextStyle(
+            fontSize: 17,
+            fontFamily: 'Montserat_Alt',
+          ),),
+        centerTitle: true,
+          backgroundColor: Colors.deepOrange,
         ),
+        body: Center(
+          child: Text('My application', style: TextStyle(
+            fontSize: 14,
+            color: Colors.blueGrey,
+            fontFamily: 'Montserat_Alt',
+            fontWeight: FontWeight.normal,
+          ),),
+        ),
+       floatingActionButton: FloatingActionButton(
+         onPressed: () {
+           print('Clicked');
+         },
+         child: Text('OK'),
+         backgroundColor: Colors.deepOrange,
+       ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
