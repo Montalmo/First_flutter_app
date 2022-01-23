1260fe4a3c2f9af2f489165859efe4821cc5df21
Montalmo <photowedg@gmail.com>
2022-01-23 14:15:02 +0200

    23.01.2022 commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,26 @@ void main() => runApp(MaterialApp(
   home: UserPanel(),
 ));
 
-class UserPanel extends StatelessWidget {
+class UserPanel extends StatefulWidget {
   const UserPanel({Key? key}) : super(key: key);
+
+  @override
+  State<UserPanel> createState() => _UserPanelState();
+}
+
+class _UserPanelState extends State<UserPanel> {
+
+  int _count = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white60,
       appBar: AppBar(
-        title: Text('Application'),
+        title: Text('Application', style: TextStyle(
+          fontSize: 17,
+          fontWeight: FontWeight.w400,
+        ),),
         centerTitle: true,
         backgroundColor: Colors.blueGrey,
       ),
@@ -21,17 +32,50 @@ class UserPanel extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              CircleAvatar(
+                Padding(padding: EdgeInsets.only(top: 32),),
+                CircleAvatar(
                 backgroundImage: AssetImage('assets/53d189dfcd54fa9ecae756ddf5a7c2ee.jpg'),
+                radius: 50,
               ),
-              Text('John Doe'),
+                Padding(padding: EdgeInsets.only(top: 16),),
+                Text('Sadovoy Andrey', style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+                color: Colors.black87,
+              ),),
+                Padding(padding: EdgeInsets.only(top: 8),),
+                Row(
+                  children: [
+                    Icon(Icons.mail_outline, size: 25,),
+                    Padding(padding: EdgeInsets.only(left: 8)),
+                    Text('asadovoy@gmail.com', style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black54
+                    ),)
+                  ],
+                ),
+                Padding(padding: EdgeInsets.only(top: 16),),
+                Text('Likes: $_count', style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.blueGrey,
+                ),),
               ],
             ),
           ],
         ),
         ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.favorite),
+        backgroundColor: Colors.redAccent,
+        onPressed: () {
+          setState(() {
+            _count++;
+          });
+        },
+      ),
     );
   }
 }
